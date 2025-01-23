958f24ceae1befd3c2f35fd14518914b711d155b
Michel Cortes Ortiz <michelcortes199@gmail.com>
2025-01-23 01:19:15 -0300

    sqlite

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,86 @@
 import { Stack } from "expo-router";
+import * as SQLite from 'expo-sqlite';
+import { Image } from "react-native";
 
+export default function Layout() {
+    
+    const db = SQLite.openDatabaseSync('nazar.db');
 
-export default function Layout(){
-    return(
-        <Stack
-            screenOptions={{
-                headerStyle:{
-                    backgroundColor: 'blue',
-                },
-                headerTintColor: 'white',
-                headerTitleStyle: {
-                    fontWeight: 'bold'
+    const createDB = async () => {
+
+        const initialEmployees = [
+            { name: 'Juan Perez'},
+            { name: 'Pedro Lopez'},
+            { name: 'Luis Nazar'},
+            { name: 'Camilo Perez'},
+            { name: 'Miguel Gonzalez'},
+          ];
+
+          db.execAsync(tx => {
+            tx.executeSql(
+                `CREATE TABLE IF NOT EXISTS Employee (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    name Text
+                );
+
+                CREATE TABLE IF NOT EXISTS File (
+                id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                name Text, 
+                data BLOB
+                );`,
+                [],
+                () => {
+                    console.log('Tablas Creadas');
+                    tx.executeSql(
+                        initialEmployees.forEach(employee => {
+                            tx.executeSql(
+                              `INSERT INTO employee (name) VALUES (?);`,
+                              [employee.name],
+                              () => console.log(`Empleado ${employee.name} insertado correctamente.`),
+                              (_, error) => console.log(`Error al insertar a ${employee.name}:`, error)
+                            );
+                        })
+                    )
                 }
-            }}
-        >
+            )
+        })
 
-            <Stack.Screen 
-                name="index"
-                options={{ title: "Home" }}
-             />
-            <Stack.Screen 
-                name="employee/view"
-                options={{ title: "Empleado" }}
-            />
+    }
 
-        </Stack>
+    return (
+        <SQLite.SQLiteProvider databaseName="nazar.db" onInit={createDB}>
+            <Stack
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: '#2A0EB7',
+                    },
+                    headerTintColor: 'white',
+                    headerTitleStyle: {
+                        fontWeight: '500'
+                    },
+                }}
+            >
+                <Stack.Screen
+                    name="index"
+                    options={{
+                        title: "Home",
+                        headerRight: () => (
+                            <Image
+                                source={require('../assets/images/logo.png')}
+                                style={{ height: 30, width: 50 }}
+                                resizeMode="contain"
+                            />
+                        ),
+                    }}
+                />
+                <Stack.Screen
+                    name="employee/view"
+                    options={{ title: "Empleado" }}
+                />
+
+            </Stack>
+        </SQLite.SQLiteProvider>
+
+
     )
 }
